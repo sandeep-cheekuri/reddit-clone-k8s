08a2cc0abdd48f15a11d51a21bf3f39146a4c74d
sandeep-cheekuri <144813740+sandeep-cheekuri@users.noreply.github.com>
2025-11-19 13:11:01 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "sandeep-s3-bucket-2225599"
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
